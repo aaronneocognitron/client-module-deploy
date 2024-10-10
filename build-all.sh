0ce9a9c4d1a2ac1e5178979b98dc91e84722efeb
darkprotocol <svechnick@mail.ru>
2024-10-10 17:51:39 +0300

    add naming changes

--- build-all.sh
+++ build-all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-GOOS=linux GOARCH=amd64 go build -o ./bin/lunix_x64 ./cmd/main.go
-GOOS=linux GOARCH=386 go build -o ./bin/lunix_x32 ./cmd/main.go
+GOOS=linux GOARCH=amd64 go build -o ./bin/linux_x64 ./cmd/main.go
+GOOS=linux GOARCH=386 go build -o ./bin/linux_x32 ./cmd/main.go
